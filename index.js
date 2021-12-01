bddab643a8d6db047fc7006b52f9eea8b3f17e49
Lenfumé Julien <julabina@hotmail.fr>
2021-12-01 10:53:39 +0100

    add display logic

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+const articlesContainer = document.querySelector(".articlesContainer");
+
 let datas;
 
 fetch("data.json")
   .then((res) => res.json())
   .then((data) => {
     datas = data;
+    displayArticles();
   });
+
+const displayArticles = () => {
+  for (let i = 0; i < datas.articles.length; i++) {
+    articlesContainer.innerHTML += `
+    <div class="article" id="${datas.articles[i].id}">
+    <h4>${datas.articles[i].name}</h4>
+    <p>${datas.articles[i].stock}</p>
+    </div>
+    `;
+  }
+};
